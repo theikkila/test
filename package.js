52e2bf141c3c8db341e70bc833bfc089d8575121
Teemu Heikkilä <sienikasvusto@pistoke.org>
2015-03-01 22:15:51 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "hello-world",
+  "private": true,
+  "version": "0.0.1",
+  "description": "Dockerfile build test",
+  "author": "Teemu Heikkilä <teemu.heikkila@pistoke.org>",
+  "dependencies": {
+    "express": "3.2.4"
+  }
+}
